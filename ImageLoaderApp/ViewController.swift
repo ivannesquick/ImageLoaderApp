aed34f4de44197cad99dc5578ee42b2a76745510
Ivan Neskin <in@decentury.io>
2022-06-22 15:15:32 +0700

    Initial Commit

---
+++ ImageLoaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLoaderApp
+//
+//  Created by Иван Нескин on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
